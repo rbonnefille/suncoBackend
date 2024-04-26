b0bb83d9c87d1363e24af05b5015488a84f31f6d
Romain Bonnefille <rbonnefille@zendesk.com>
2024-04-26 15:17:16 +0200

    Update Vite configuration and dependencies

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,35 +1,38 @@
-import { fileURLToPath, URL } from "node:url";
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import Components from "unplugin-vue-components/vite";
-import VueRouter from "unplugin-vue-router/vite";
-import Layouts from "vite-plugin-vue-layouts";
+import { fileURLToPath, URL } from 'node:url';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import Components from 'unplugin-vue-components/vite';
+import VueRouter from 'unplugin-vue-router/vite';
+import Layouts from 'vite-plugin-vue-layouts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   // base: "/vue-sunco-dashboard/",
   plugins: [
     VueRouter({
-      routesFolder: "src/pages",
-      extensions: [".vue"],
-      importMode: "async",
-      dts: "./typed-router.d.ts",
+      routesFolder: 'src/pages',
+      extensions: ['.vue'],
+      importMode: 'async',
+      dts: './typed-router.d.ts',
     }),
     Layouts(),
     vue(),
-    Components({ dirs: ["./src/components"] }),
+    Components({ dirs: ['./src/components'] }),
   ],
   server: {
+    fs: {
+      cachedChecks: false,
+    },
     proxy: {
-      "/auth": "http://127.0.0.1:3000/",
-      "/integrations": "http://127.0.0.1:3000/",
-      "/users": "http://127.0.0.1:3000/",
-      "/integrations/sbintegrations": "http://127.0.0.1:3000/",
+      '/auth': 'http://127.0.0.1:3000/',
+      '/integrations': 'http://127.0.0.1:3000/',
+      '/users': 'http://127.0.0.1:3000/',
+      '/integrations/sbintegrations': 'http://127.0.0.1:3000/',
     },
   },
   resolve: {
     alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 });
