8be130cdb34189622ea10827033ef3cef9e81892
Romain Bonnefille <rbonnefille@zendesk.com>
2024-05-17 10:56:08 +0200

    chore: Refactor SuncoRoutes component and update dependencies

--- client/src/components/SuncoRoutes.vue
+++ client/src/components/SuncoRoutes.vue
@@ -1,60 +1,49 @@
 <template>
   <h4 class="h4">Availble routes to access SunCo data</h4>
   <div class="p-3 mb-3 m-0 d-inline-flex list-group">
-    <RouterLink
-      v-if="authenticated"
-      id="getUser"
-      target="_blank"
-      :to="`/sunco/users/${external_id}`"
-      class="list-group-item list-group-item-action"
-      >Get User data: {{ external_id }}
+    <RouterLink v-if="authenticated" id="getUser" target="_blank" :to="`/sunco/users/${external_id}`"
+      class="list-group-item list-group-item-action">Get User data: {{ external_id }}
     </RouterLink>
-    <RouterLink
-      v-for="(route, index) in additionalRoutes"
-      :key="route.to"
-      :to="route.to"
-      :target="route.target"
-      class="list-group-item list-group-item-action"
-      v-bind="$attrs"
-    >
+    <RouterLink v-for="(route, index) in additionalRoutes" :key="route.to" :to="route.to" :target="route.target"
+      class="list-group-item list-group-item-action" v-bind="$attrs">
       {{ route.text }}
     </RouterLink>
   </div>
 </template>
 
 <script setup>
-import { useUserStore } from '@/stores/store';
-import { storeToRefs } from 'pinia';
+import { useUserStore } from "@/stores/store";
+import { storeToRefs } from "pinia";
 
 const userStore = useUserStore();
 const { authenticated, external_id } = storeToRefs(userStore);
 
 const additionalRoutes = [
   {
-    to: '/',
-    target: '',
-    text: 'Go to Home page',
+    to: "/",
+    target: "",
+    text: "Go to Home page",
   },
   {
-    to: '/sunco/integrations',
-    target: '_blank',
-    text: 'List integrations',
+    to: "/sunco/integrations",
+    target: "_blank",
+    text: "List integrations",
   },
   {
-    to: '/sunco/integrations/switchboard',
-    target: '_blank',
-    text: 'Switchboard Configuration',
+    to: "/sunco/integrations/switchboard",
+    target: "_blank",
+    text: "Switchboard Configuration",
   },
   {
-    to: '/sunco/snippets/web-widget',
-    target: '_blank',
-    text: 'SunCo Web Widget Snippets',
+    to: "/sunco/snippets/web-widget",
+    target: "_blank",
+    text: "SunCo Web Widget Snippets",
   },
   {
-    to: '/sunco/users',
-    target: '_blank',
-    text: 'Search a SunCo User',
-  },
+    to: "/sunco/users",
+    target: "_blank",
+    text: "Search a SunCo User",
+  }
 ];
 </script>
 
