a9718d09c43104125cc37e725de958afca1e1477
Romain Bonnefille <rbonnefille@zendesk.com>
2024-05-17 10:56:48 +0200

    refactor: Improve loading state in VDataItem and VSelect components

--- client/src/components/VSelect.vue
+++ client/src/components/VSelect.vue
@@ -1,11 +1,7 @@
 <template>
   <label class="form-label">{{ label }}</label>
-  <select
-    :value="modelValue"
-    class="form-select form-select-sm"
-    @change="$emit('update:modelValue', $event.target.value)"
-    v-bind="$attrs"
-  >
+  <select :value="modelValue" class="form-select form-select-sm"
+    @change="$emit('update:modelValue', $event.target.value)" v-bind="$attrs">
     <option disabled value="">{{ optionHint }}</option>
     <option v-for="option in options" :key="option.id" :value="option.id">
       {{ option.name || option.id || option }}
@@ -14,12 +10,11 @@
 </template>
 
 <script setup>
-defineEmits(['update:modelValue']);
+defineEmits(["update:modelValue"]);
 
 defineProps({
   label: {
     type: String,
-    required: true,
   },
   modelValue: {
     type: [String, Number, Boolean, Object, Array],
@@ -27,7 +22,7 @@ defineProps({
   },
   optionHint: {
     type: String,
-    default: 'Please select one',
+    default: "Please select one",
   },
   options: {
     type: Array,
